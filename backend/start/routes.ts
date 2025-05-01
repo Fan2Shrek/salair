0c5361f4ec34b1afe29c455757d3236572d11824
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-01 19:59:09 +0200

    fix(backend:auth): correct logout route definition in routes

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -21,7 +21,7 @@ router
   .group(() => {
     router.post('/register', [AuthController, 'register']).as('auth.register')
     router.post('/login', [AuthController, 'login']).as('auth.login')
-    router.delete('/login', [AuthController, 'logout']).as('auth.logout')
+    router.delete('/logout', [AuthController, 'logout']).as('auth.logout')
     router.get('/me', [AuthController, 'me']).as('auth.me').use(middleware.auth())
     router.post('/refresh', [AuthController, 'refresh']).as('auth.refresh')
   })
