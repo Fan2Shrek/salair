6c4bf2bb0c9e69d58de79553caebc3952097f175
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-06 20:47:08 +0200

    feat(frontend:footer): add UFooter component with navigation and subscription form

---
+++ frontend/components/organisms/landing/UFooter.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts"></script>
+
+<template>
+    <footer class="w-full pt-16 pb-12 bg-secondary">
+        <div class="max-w-7xl w-full mx-auto">
+            <div class="w-full flex justify-between">
+                <div class="space-y-8">
+                    <div
+                        class="flex items-center gap-3 rounded-lg hover:bg-primary-hover p-2 cursor-pointer w-fit"
+                        @click="navigateTo('/')"
+                    >
+                        <ULogo class="size-8" alt="Logo Salair" />
+                        <p class="text-primary font-semibold text-lg">Salair</p>
+                    </div>
+                    <nav>
+                        <ul class="w-full flex items-center gap-8">
+                            <li>
+                                <ULink to="#" variant="secondary">{{ $t('header.navigation.product') }}</ULink>
+                            </li>
+                            <li>
+                                <ULink to="#" variant="secondary">{{ $t('header.navigation.about') }}</ULink>
+                            </li>
+                            <li>
+                                <ULink to="/pricing" variant="secondary">{{ $t('header.navigation.pricing') }}</ULink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="space-y-4">
+                    <h4 class="font-semibold text-primary text-sm">Stay up to date</h4>
+                    <form class="flex items-center gap-4">
+                        <UInput type="email" name="email" placeholder="Enter your email" />
+                        <UButton size="lg">Subscribe</UButton>
+                    </form>
+                </div>
+            </div>
+            <div class="mt-16 pt-8 flex justify-between border-t border-secondary">
+                <p class="text-quaternary">© {{ new Date().getFullYear() }} Salair. {{ $t('copyright') }}</p>
+                <nav>
+                    <ul class="flex items-center gap-4">
+                        <li>
+                            <NuxtLink to="#" variant="secondary" class="!text-quaternary">Terms</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="#" variant="secondary" class="!text-quaternary">Privacy</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/pricing" variant="secondary" class="!text-quaternary ">Cookies</NuxtLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </footer>
+</template>
+
