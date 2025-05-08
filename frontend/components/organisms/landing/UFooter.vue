bcce47c2c2003bc369fe847ff660b78fcda0b41e
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-08 19:23:40 +0200

    style(components:footer): adjust padding and layout for improved responsiveness

--- frontend/components/organisms/landing/UFooter.vue
+++ frontend/components/organisms/landing/UFooter.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-    <footer class="w-full pt-16 pb-12 bg-secondary">
+    <footer class="w-full pt-12 md:pt-16 pb-12 bg-secondary">
         <div class="max-w-7xl w-full mx-auto">
-            <div class="w-full flex justify-between">
-                <div class="space-y-8">
+            <div class="w-full md:flex justify-between">
+                <div class="space-y-8 max-md:px-4">
                     <div
                         class="flex items-center gap-3 rounded-lg hover:bg-primary-hover p-2 cursor-pointer w-fit"
                         @click="navigateTo('/')"
@@ -26,15 +26,15 @@
                         </ul>
                     </nav>
                 </div>
-                <div class="space-y-4">
+                <div class="space-y-4 max-md:mt-12 max-md:px-4">
                     <h4 class="font-semibold text-primary text-sm">Stay up to date</h4>
                     <form class="flex items-center gap-4">
-                        <UInput type="email" name="email" placeholder="Enter your email" />
+                        <UInput type="email" name="email" placeholder="Enter your email" class="max-md:flex-grow"/>
                         <UButton size="lg">Subscribe</UButton>
                     </form>
                 </div>
             </div>
-            <div class="mt-16 pt-8 flex justify-between border-t border-secondary">
+            <div class="mt-16 pt-8 flex max-md:flex-col-reverse md:justify-between max-md:gap-4 border-t border-secondary max-md:px-4">
                 <p class="text-quaternary">© {{ new Date().getFullYear() }} Salair. {{ $t('copyright') }}</p>
                 <nav>
                     <ul class="flex items-center gap-4">
