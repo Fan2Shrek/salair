c5db57b29a7e446678ae97c6a8382e4c7509250a
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-08 18:34:46 +0200

    feat(frontend:doc): add sidebar component and layout for documentation pages

---
+++ frontend/components/organisms/doc/USidebar.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+    import HomeIcon from '~/components/atoms/icons/HomeIcon.vue';
+    import SearchIcon from '~/components/atoms/icons/SearchIcon.vue';
+
+    const componentsList = [
+        { name: 'Avatar', category: 'atoms', path: '/doc/components/avatar' },
+        { name: 'Badge', category: 'atoms', path: '/doc/components/badge' },
+        { name: 'Button', category: 'atoms', path: '/doc/components/button' },
+        { name: 'Checkbox', category: 'atoms', path: '/doc/components/checkbox' },
+        { name: 'Divider', category: 'atoms', path: '/doc/components/divider' },
+        { name: 'FeaturedIcon', category: 'atoms', path: '/doc/components/featured-icon' },
+        { name: 'Input', category: 'atoms', path: '/doc/components/input' },
+        { name: 'InputNumber', category: 'atoms', path: '/doc/components/input-number' },
+        { name: 'Link', category: 'atoms', path: '/doc/components/link' },
+        { name: 'Loading', category: 'atoms', path: '/doc/components/loading' },
+        { name: 'Logo', category: 'atoms', path: '/doc/components/logo' },
+        { name: 'SelectBox', category: 'atoms', path: '/doc/components/select-box' },
+        { name: 'SidebarItem', category: 'atoms', path: '/doc/components/sidebar-item' },
+        { name: 'Switch', category: 'atoms', path: '/doc/components/switch' },
+
+        { name: 'Accordion', category: 'molecules', path: '/doc/components/accordion' },
+        { name: 'BaseModal', category: 'molecules', path: '/doc/components/base-modal' },
+        { name: 'DatePicker', category: 'molecules', path: '/doc/components/date-picker' },
+        { name: 'Dropdown', category: 'molecules', path: '/doc/components/dropdown' },
+        { name: 'LanguageSwitcher', category: 'molecules', path: '/doc/components/language-switcher' },
+        { name: 'Tabs', category: 'molecules', path: '/doc/components/tabs' },
+        { name: 'ToastContainer', category: 'molecules', path: '/doc/components/toast-container' },
+
+        { name: 'Calendar', category: 'organisms', path: '/doc/components/calendar' },
+        { name: 'CloseButton', category: 'organisms', path: '/doc/components/close-button' },
+        { name: 'LogoutButton', category: 'organisms', path: '/doc/components/logout-button' },
+        { name: 'Table', category: 'organisms', path: '/doc/components/table' },
+        { name: 'Sidebar', category: 'organisms', path: '/doc/components/sidebar' },
+    ];
+
+    const groupedComponents = {
+        atoms: componentsList.filter((comp) => comp.category === 'atoms'),
+        molecules: componentsList.filter((comp) => comp.category === 'molecules'),
+        organisms: componentsList.filter((comp) => comp.category === 'organisms'),
+    };
+</script>
+
+<template>
+    <aside class="fixed top-0 left-0 h-full border-r border-secondary bg-primary max-w-xs w-full overflow-y-auto">
+        <div class="pt-6">
+            <div class="px-5 space-y-5">
+                <div
+                    class="flex items-center gap-3 rounded-lg hover:bg-primary-hover p-2 cursor-pointer"
+                    @click="navigateTo('/')"
+                >
+                    <ULogo class="size-8" alt="Logo Salair" />
+                    <p class="text-primary font-semibold text-lg">Salair</p>
+                </div>
+                <form class="w-full">
+                    <UInput
+                        type="search"
+                        :icon="SearchIcon"
+                        icon-position="leading"
+                        class="w-full"
+                        placeholder="Search"
+                    />
+                </form>
+            </div>
+            <div class="mt-4 px-4">
+                <nav class="w-full">
+                    <ul class="w-full">
+                        <USidebarItem title="Home" :icon="HomeIcon" to="/doc/components" />
+                    </ul>
+                </nav>
+            </div>
+            <div class="px-5 py-2">
+                <UDivider class="w-full" />
+            </div>
+            <div class="mt-2 px-4">
+                <nav class="w-full">
+                    <ul class="w-full">
+                        <USidebarItem title="Atoms" collapsible>
+                            <li>
+                                <ul>
+                                    <USidebarItem
+                                        v-for="component in groupedComponents.atoms"
+                                        :key="component.name"
+                                        :title="component.name"
+                                        :to="component.path"
+                                    />
+                                </ul>
+                            </li>
+                        </USidebarItem>
+
+                        <USidebarItem title="Molecules" collapsible>
+                            <li>
+                                <ul>
+                                    <USidebarItem
+                                        v-for="component in groupedComponents.molecules"
+                                        :key="component.name"
+                                        :title="component.name"
+                                        :to="component.path"
+                                    />
+                                </ul>
+                            </li>
+                        </USidebarItem>
+
+                        <USidebarItem title="Organisms" collapsible>
+                            <li>
+                                <ul>
+                                    <USidebarItem
+                                        v-for="component in groupedComponents.organisms"
+                                        :key="component.name"
+                                        :title="component.name"
+                                        :to="component.path"
+                                    />
+                                </ul>
+                            </li>
+                        </USidebarItem>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </aside>
+</template>
+
---
+++ frontend/layouts/doc.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full h-full flex">
+        <USidebar />
+        <div class="pl-80 h-full w-full">
+            <slot />
+        </div>
+    </div>
+</template>
