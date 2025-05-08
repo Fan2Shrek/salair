f93b03f5d6cc2a6b027d5e3ba9ebd1bcf4a26a52
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-08 13:29:50 +0200

    feat(routes): organize user routes and improve route structure

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -11,6 +11,7 @@ const AuthController = () => import('#controllers/auth_controller')
 const PlansController = () => import('#controllers/plans_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const UsersController = () => import('#controllers/users_controller')
 
 router.get('/', async () => {
   return {
@@ -18,16 +19,32 @@ router.get('/', async () => {
   }
 })
 
+// Authentication routes
 router
   .group(() => {
-    // Authentication routes
     router.post('/register', [AuthController, 'register']).as('auth.register')
     router.post('/login', [AuthController, 'login']).as('auth.login')
     router.delete('/logout', [AuthController, 'logout']).as('auth.logout')
     router.get('/me', [AuthController, 'me']).as('auth.me').use(middleware.auth())
     router.post('/refresh', [AuthController, 'refresh']).as('auth.refresh')
+  })
+  .prefix('/api')
 
-    // Plans routes
+// Plans routes
+router
+  .group(() => {
     router.get('/plans', [PlansController, 'index']).as('plans.index')
   })
   .prefix('/api')
+
+// Users routes
+router
+  .group(() => {
+    router.get('users', [UsersController, 'index'])
+    router.get('users/:id', [UsersController, 'show'])
+    router.post('users', [UsersController, 'store'])
+    router.put('users/:id', [UsersController, 'update'])
+    router.delete('users/:id', [UsersController, 'destroy'])
+  })
+  .use([middleware.auth(), middleware.admin()])
+  .prefix('api')
