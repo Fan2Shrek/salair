6b8cf4aa8b5d1ff2c0a0b52580a3cff5123b51a2
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-13 18:20:48 +0200

    feat(frontend:progress): add UProgress component for visual progress indication

---
+++ frontend/pages/doc/progress.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    const value = ref(40);
+
+
+</script>
+
+<template>
+    <NuxtLayout name="doc">
+        <main class="flex flex-col gap-4 items-center justify-center max-w-6xl mx-auto w-full h-full">
+            <UInputNumber v-model="value" :max="100" />
+
+            <UProgress :value="value" :max="100" />
+        </main>
+    </NuxtLayout>
+</template>
+
