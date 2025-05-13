31e9535143abd0a1b850bea409bc59af8c4438f9
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-13 11:17:31 +0200

    feat(tooltip): create tooltip component with basic structure and layout

---
+++ frontend/pages/doc/tooltip.vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLayout name="doc">
+        <main class="h-full flex items-center justify-center">
+            <UTooltip text="Ceci est une info-bulle">
+                <UButton>Survolez-moi</UButton>
+            </UTooltip>
+        </main>
+    </NuxtLayout>
+</template>
+
