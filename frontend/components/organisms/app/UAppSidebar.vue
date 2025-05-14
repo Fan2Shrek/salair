5474516c3dc247cfd6772aba859d6cc4186a5211
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-14 16:33:21 +0200

    feat(frontend:sidebar): enhance UAppSidebar with user avatar and details display

--- frontend/components/organisms/app/UAppSidebar.vue
+++ frontend/components/organisms/app/UAppSidebar.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import HomeIcon from '~/components/atoms/icons/HomeIcon.vue';
-import SearchIcon from '~/components/atoms/icons/SearchIcon.vue';
+    import HomeIcon from '~/components/atoms/icons/HomeIcon.vue';
+    import SearchIcon from '~/components/atoms/icons/SearchIcon.vue';
 
+    const authStore = useAuthStore()
 </script>
 
 <template>
     <aside class="fixed top-0 left-0 h-full border-r border-secondary bg-primary max-w-xs w-full overflow-y-auto">
-        <div class="pt-6">
+        <div class="pt-6 flex flex-col h-full">
             <div class="px-5 space-y-5">
                 <div
                     class="flex items-center gap-3 rounded-lg hover:bg-primary-hover p-2 cursor-pointer"
@@ -25,13 +26,25 @@ import SearchIcon from '~/components/atoms/icons/SearchIcon.vue';
                     />
                 </form>
             </div>
-            <div class="mt-4 px-4">
+            <div class="mt-4 px-4 flex-grow">
                 <nav class="w-full">
                     <ul class="w-full">
                         <USidebarItem title="Home" :icon="HomeIcon" to="/" />
                     </ul>
                 </nav>
+                <UDivider class="mt-1" />
+            </div>
+            <div class="px-4 pb-6">
+                <div class="rounded-xl border border-secondary flex gap-2 p-3">
+                    <UAvatar v-if="authStore.user?.avatar" size="md" :image-src="authStore.user?.avatar" />
+                    <UAvatar v-else size="md" :text="authStore.user!.firstName[0] + authStore.user!.lastName[0]"  />
+                    <div class="flex-grow">
+                        <p class="font-semibold text-primary text-sm">{{ authStore.user?.firstName }} {{ authStore.user?.lastName }}</p>
+                        <p class="text-tertiary text-sm">{{ authStore.user?.email }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </aside>
 </template>
+
