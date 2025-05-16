a49a49a6ad7522eb332c31340fc0e8f271c54121
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-16 09:49:55 +0200

    fix(studio): add gitinfo params to nuxt content

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -73,7 +73,12 @@ export default defineNuxtConfig({
     },
     content: {
         preview: {
-            api: 'https://api.nuxt.studio'
+            api: 'https://api.nuxt.studio',
+            gitInfo: {
+                name: "salair",
+                owner: "nassimlnd",
+                url: "https://github.com/nassimlnd/salair"
+            }
         }
     }
 });
