67f504059b7d8dc0e922d3ab6eedb397ffe8b535
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-18 17:22:38 +0200

    feat(mail): send confirmation email for contact requests

--- backend/app/controllers/inbound_mails_controller.ts
+++ backend/app/controllers/inbound_mails_controller.ts
@@ -1,5 +1,6 @@
 import InboundMail from '#models/inbound_mail'
 import type { HttpContext } from '@adonisjs/core/http'
+import mail from '@adonisjs/mail/services/main'
 
 export default class InboundMailsController {
   async receive({ request, response }: HttpContext) {
@@ -12,6 +13,23 @@ export default class InboundMailsController {
       text: data['body-plain'],
       html: data['html-plain'],
     })
+
+    if (inboudMail.to === 'contact@salair.fr') {
+      await mail.send((message) => {
+        message
+          .to(inboudMail.from)
+          .from('Support Salair <contact@salair.fr>')
+          .subject('Demande de contact reçue !')
+          .text(
+            `Bonjour, 
+            
+            Votre demande de contact a bien été reçue. Elle sera traitée rapidement par un membre de notre équipe.
+            
+            Cordialement,
+            L'équipe Salair`
+          )
+      })
+    }
 
     console.log(`📥 Mail recevied from ${inboudMail.from}`)
 
