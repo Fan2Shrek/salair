68be62df7f5324ebe27dbb94e6dd8cfd3ebcd0d6
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-20 01:44:29 +0200

    refactor(useAuthFetch): simplify finalOptions creation for better readability

--- frontend/composables/useAuthFetch.ts
+++ frontend/composables/useAuthFetch.ts
@@ -4,27 +4,30 @@ export async function useAuthFetch<T>(url: string, options: UseFetchOptions<T> =
     await nextTick();
 
     const authStore = useAuthStore();
-    const headers = {
-        'Authorization': `Bearer ${authStore.accessToken}`
-    }
-
-    const finalOptions: UseFetchOptions<T> = {
-        ...options,
-        headers: {
-            ...headers,
-            ...(options.headers || {}),
-        },
-        credentials: 'include',
-        immediate: true,
+    const finalOptions = (): UseFetchOptions<T> => {
+        // Création dynamique des options avec le token actuel
+        const headers = {
+            'Authorization': `Bearer ${authStore.accessToken}`
+        };
+        
+        return {
+            ...options,
+            headers: {
+                ...headers,
+                ...(options.headers || {}),
+            },
+            credentials: 'include',
+            immediate: true,
+        };
     };
 
-    let response = await useFetch(url, finalOptions);
+    let response = await useFetch(url, finalOptions());
 
     if (response.error.value?.statusCode === 401) {
         const refreshed = await authStore.refresh();
 
         if (refreshed) {
-            response = await useAuthFetch(url, finalOptions);
+            response = await useFetch(url, finalOptions());
         } else {
             await authStore.logout();
             navigateTo('/login');
