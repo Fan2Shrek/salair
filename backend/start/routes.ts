4901fa6c521fe170357745de9b4fcdd966ef95b0
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-20 14:11:28 +0200

    fix(routes): correct HTML syntax in root route greeting message

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -19,7 +19,7 @@ import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
-    hello: 'Bonjour pied (<a href="https://github.com/Fan2Shrek">Le bg ultime</a>',
+    hello: 'Bonjour pied <a href="https://github.com/Fan2Shrek">Le bg ultime</a>',
   }
 })
 
