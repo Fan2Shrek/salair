39929f983d6c5ba772fd1a16e650de0b3baf25b9
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-21 16:41:39 +0200

    feat(frontend:components): add URoundSelector component template

---
+++ frontend/components/organisms/app/settings/URoundSelector.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="absolute bottom-3 left-3 bg-brand-solid size-5 rounded-full flex items-center justify-center">
+        <div class="bg-white size-2 rounded-full" />
+    </div>
+</template>
+
