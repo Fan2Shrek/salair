d674e66c4e679a77197b7e1dcad7314aad041f0a
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-22 22:43:44 +0200

    refactor(routes): remove unused test route for password reset

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -102,7 +102,3 @@ router
       .use([middleware.auth(), middleware.admin()])
   })
   .prefix('api')
-
-router.get('/test', ({ view }) => {
-  return view.render('mails/reset_password', { code: 123456, user: { firstName: 'Nassim' } })
-})
