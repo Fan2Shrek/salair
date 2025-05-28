61893fb28933bcf9344d768703e564c10cc87321
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-28 13:33:03 +0200

    feat(sitemap): use runtime config for fetching blog slugs

--- frontend/server/api/__sitemap__/urls.ts
+++ frontend/server/api/__sitemap__/urls.ts
@@ -2,7 +2,9 @@ import { defineSitemapEventHandler } from '#imports'
 import type { SitemapUrlInput } from '#sitemap/types'
 
 export default defineSitemapEventHandler(async () => {
-    const res = await fetch('https://api.salair.fr/api/blog/slugs')
+    const runtimeConfig = useRuntimeConfig()
+    
+    const res = await fetch(`${runtimeConfig.public.apiUrl}/api/blog/slugs`)
     const slugs = await res.json()
     console.log(slugs)
     const urls = slugs.map((s: {slug: string}) => {
