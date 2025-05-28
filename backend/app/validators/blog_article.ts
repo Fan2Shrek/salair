025cf5e416578f55c6cd1ca4c466e58884d6ae04
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-05-28 13:26:02 +0200

    feat(validator): add uniqueness validation for blog article slug

--- backend/app/validators/blog_article.ts
+++ backend/app/validators/blog_article.ts
@@ -2,7 +2,11 @@ import vine from '@vinejs/vine'
 
 export const blogArticleValidator = vine.compile(
   vine.object({
-    slug: vine.string(),
+    slug: vine.string().unique(async (db, value) => {
+      const match = await db.from('blog_articles').where('slug', value)
+
+      return !match
+    }),
     visible: vine.boolean(),
     status: vine.enum(['draft', 'published', 'archived']),
   })
