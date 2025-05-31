bb43871735be856e43a670d3992a443f58e5fadb
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-06-01 01:06:46 +0200

    fix(access-token-manager): update user type casting in createToken method

--- backend/app/contexts/user-management/infrastructure/adapters/access_token_manager.adapter.ts
+++ backend/app/contexts/user-management/infrastructure/adapters/access_token_manager.adapter.ts
@@ -2,7 +2,6 @@ import { AccessTokenManagerContract } from '#contexts/user-management/applicatio
 import { UserIdentifier } from '#contexts/user-management/domain/value-objects/user_identifier'
 import { HttpContext } from '@adonisjs/core/http'
 import UserModel from '#contexts/user-management/infrastructure/database/models/user.model'
-import User from '#models/user'
 
 export class AccessTokenManagerAdapter implements AccessTokenManagerContract {
   #ctx: HttpContext
@@ -18,7 +17,7 @@ export class AccessTokenManagerAdapter implements AccessTokenManagerContract {
       throw new Error('User does not exist')
     }
 
-    const token = await this.#ctx.auth.use('api').createToken(user as User)
+    const token = await this.#ctx.auth.use('api').createToken(user as any)
 
     return token.value!.valueOf()
   }
