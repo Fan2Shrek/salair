fffef2324d7f1a54b249194409e60060b7426198
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-06-12 14:31:52 +0200

    feat(seeder): add new blog articles with detailed descriptions and statuses

--- backend/database/seeders/004_article_seeder.ts
+++ backend/database/seeders/004_article_seeder.ts
@@ -7,13 +7,24 @@ export default class extends BaseSeeder {
     const user = await User.query().where('email', 'nassim@mail.com').firstOrFail()
 
     await BlogArticle.create({
-      title: 'Facture autoentrepreneur',
-      description: 'Description test',
+      title: 'Comment créer une facture en autoentreprise ?',
+      description:
+        "Découvrez les étapes pour générer une facture conforme en tant qu'autoentrepreneur, avec les mentions obligatoires.",
       slug: 'facture-autoentrepreneur',
       visible: true,
       status: 'published',
       authorId: user.id,
       mainPicture: 'https://storage.salair.fr/salair/public/image-facture-autoentrepreneur.jpg',
+    })
+
+    await BlogArticle.create({
+      title: 'Comment déclarer son chiffre d’affaires à l’URSSAF ?',
+      description:
+        "Déclarer son chiffre d'affaires à l'URSSAF est une obligation mensuelle ou trimestrielle pour les autoentrepreneurs. Voici comment faire simplement et éviter les erreurs.",
+      slug: 'declaration-urssaf',
+      visible: true,
+      status: 'draft',
+      authorId: user.id,
     })
   }
 }
