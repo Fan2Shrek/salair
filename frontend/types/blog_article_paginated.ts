b56520e5ccb5df836d8be240807cff535a267afb
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-06-12 14:32:54 +0200

    feat(types:blog_article_paginated): add BlogArticlePaginated interface

---
+++ frontend/types/blog_article_paginated.ts
@@ -0,0 +1,16 @@
+import type { BlogArticle } from "./blog_article";
+
+export interface BlogArticlePaginated {
+    data: BlogArticle[]
+    meta: {
+        total: number
+        currentPage: number
+        firstPage: number
+        lastPage: number
+        firstPageUrl: string
+        lastPageUrl: string
+        nextPageUrl: string | null
+        previousPageUrl: string | null
+        perPage: number
+    }
+}
