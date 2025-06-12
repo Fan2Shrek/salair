09b57b54b6248b4e361dbbb4b6194bb5cafcdd48
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-06-12 17:47:40 +0200

    feat(articles): add publish functionality to update article status

--- frontend/composables/admin/useArticles.ts
+++ frontend/composables/admin/useArticles.ts
@@ -1,3 +1,4 @@
+import type { BlogArticle } from "~/types/blog_article";
 import type { BlogArticlePaginated } from "~/types/blog_article_paginated";
 
 export default function useBlog() {
@@ -21,8 +22,30 @@ export default function useBlog() {
         }
     }
 
+    const publish = async (articleId: string) => {
+        try {
+            const { data, error } = await useAuthFetch<BlogArticle>($api(`/api/admin/articles/${articleId}`), {
+                method: 'PUT',
+                body: {
+                    status: 'published'
+                }
+            })
+
+            if (data.value && !error.value) {
+                toast.success("L'article a bien été publié", `L'article ${data.value.slug} a bien été publié.`)
+            }
+
+            if (error.value) {
+                toast.error('An error occured', error.value.message)
+            }
+        } catch {
+            toast.error('An error occured', '')
+        }
+    }
+
     return {
         articles,
-        loadContent
+        loadContent,
+        publish
     }
 }
