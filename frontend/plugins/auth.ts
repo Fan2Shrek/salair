55ac1ce7a4941d343873816e1707170ceec030eb
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-06-18 14:26:00 +0200

    fix(auth): simplify user fetching logic by removing unnecessary user check

--- frontend/plugins/auth.ts
+++ frontend/plugins/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(async () => {
     const authStore = useAuthStore();
 
-    if (!authStore.user && import.meta.client && authStore.isAuthenticated) {
+    if (import.meta.client && authStore.isAuthenticated) {
         await authStore.fetchUser();
     }
 });
