47c16ade7638e846ea32038d1bc8913f17018a3d
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-07-03 16:11:08 +0200

    feat: implement LogoService for API interactions with Logo.dev

---
+++ backend/app/services/logo.service.ts
@@ -0,0 +1,22 @@
+import env from '#start/env'
+import { LogoResponseDto } from '../../types/dtos/logo_response.dto.js'
+
+/**
+ * Service responsible for communicating with the Logo.dev service.
+ * Handles logo-related operations and API interactions with Logo.dev.
+ */
+export class LogoService {
+  private static readonly TOKEN = env.get('API_SIRENE_TOKEN', '')
+
+  static async search(query: string) {
+    const response = await fetch(encodeURI(`https://api.logo.dev/search?q=${query}`), {
+      headers: {
+        Authorization: `Bearer: ${this.TOKEN}`,
+      },
+    })
+
+    const data = await response.json()
+
+    return data as LogoResponseDto
+  }
+}
